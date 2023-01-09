a5ea4840752418407bf960d7c53e4e425ca89dd4
KarthikKumar <karthikganachari@gmail.com>
2023-01-09 16:42:05 +0530

    FlipkartCustomerProfiles Developed

---
+++ GetCustomerProfileIds/src/main/java/FlipkartCustomerProfiles/App.java
@@ -0,0 +1,13 @@
+package FlipkartCustomerProfiles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GetCustomerProfileIds/src/test/java/FlipkartCustomerProfiles/AppTest.java
@@ -0,0 +1,20 @@
+package FlipkartCustomerProfiles;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
